ba0ea4b091aaa524ecf503a1df5cc205855d4143
William Dussault <dalloriam@gmail.com>
2020-07-28 19:34:33 -0400

    Ditch variants entirely

--- protocol/src/lib.rs
+++ protocol/src/lib.rs
@@ -2,10 +2,8 @@
 
 mod action_manifest;
 mod trigger;
-mod variant;
 
 pub type RuleID = u64;
 
 pub use action_manifest::ActionManifest;
 pub use trigger::{Trigger, TriggerConfiguration};
-pub use variant::Variant;
