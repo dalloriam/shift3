78fad88b0075e03cfb34c2f5dd4859e5d6acd629
William Dussault <dalloriam@gmail.com>
2020-08-25 21:18:51 -0400

    Add some FTs

--- process/src/process/config/trigger.rs
+++ process/src/process/config/trigger.rs
@@ -12,13 +12,15 @@ use trigger_system::{
     TriggerConfigLoader, TriggerQueueWriter, TriggerSystem,
 };
 
-#[derive(Deserialize, Serialize)]
+/// Configuration struct of the trigger system.
+#[derive(Deserialize, Serialize, Debug, PartialEq)]
 pub struct TriggerSystemConfiguration {
     config_reader: ConfigReaderConfiguration,
     queue_writer: QueueWriterConfiguration,
 }
 
 impl TriggerSystemConfiguration {
+    /// Converts the trigger system configuration to a usable service instance.
     pub fn into_instance(self) -> Result<Service> {
         let cfg_loader = self.config_reader.into_instance()?;
         let queue_writer = self.queue_writer.into_instance()?;
@@ -26,7 +28,10 @@ impl TriggerSystemConfiguration {
     }
 }
 
-#[derive(Deserialize, Serialize)]
+/// Configuration of the trigger config reader.
+///
+/// Contains configurations for the various supported config readers (e.g. disk, datastore).
+#[derive(Deserialize, Serialize, Debug, PartialEq)]
 #[serde(tag = "type")] // ConfigReaderConfiguration::Disk{blah: blah} => {type: Disk, blah: blah}
 pub enum ConfigReaderConfiguration {
     DataStore {
@@ -39,7 +44,7 @@ pub enum ConfigReaderConfiguration {
 }
 
 impl ConfigReaderConfiguration {
-    /// Returns a usable trigger config loader from the configuration.
+    /// Returns a usable trigger config loader from the configuration struct.
     pub fn into_instance(self) -> Result<Box<dyn TriggerConfigLoader + Send>> {
         match self {
             ConfigReaderConfiguration::File { file } => {
@@ -56,7 +61,7 @@ impl ConfigReaderConfiguration {
     }
 }
 
-#[derive(Deserialize, Serialize)]
+#[derive(Deserialize, Serialize, Debug, PartialEq)]
 #[serde(tag = "type")]
 pub enum QueueWriterConfiguration {
     Directory {
@@ -85,5 +90,92 @@ impl QueueWriterConfiguration {
                 topic,
             )?)),
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::path::PathBuf;
+
+    use super::{ConfigReaderConfiguration, QueueWriterConfiguration};
+
+    macro_rules! parse_ok {
+        ($t: ident, $(($name:ident, $eq_to:expr),)*) => {
+            $(
+                #[test]
+                fn $name() {
+                    const DATA_RAW: &str =
+                        include_str!(concat!("test_data/", stringify!($name), ".json"));
+
+                    let deserialized: $t = serde_json::from_str(DATA_RAW).unwrap();
+                    assert_eq!(deserialized, $eq_to);
+                }
+            )*
+        };
+    }
+
+    macro_rules! parse_fail {
+        ($t: ident, $($name:ident,)*) => {
+            $(
+                #[test]
+                fn $name() {
+                    const DATA_RAW: &str =
+                        include_str!(concat!("test_data/", stringify!($name), ".json"));
+
+                    let deserialized_maybe: Result<$t, serde_json::Error> =
+                        serde_json::from_str(DATA_RAW);
+                    assert!(deserialized_maybe.is_err());
+                }
+            )*
+        };
+    }
+
+    // Validates successful parsing of config reader configurations.
+    parse_ok! {
+        ConfigReaderConfiguration,
+
+        // File config.
+        (file_cfg_valid, ConfigReaderConfiguration::File{file: PathBuf::from("/tmp/yeet.json")}),
+
+        // Datastore config.
+        (
+            ds_cfg_valid,
+            ConfigReaderConfiguration::DataStore{
+                project_id: String::from("asdf123"),
+                credentials_file_path: String::from("/etc/gcloud.json")
+            }
+        ),
+    }
+
+    // Validates that invalid configs fail, as expected.
+    parse_fail! {
+        ConfigReaderConfiguration,
+
+        file_cfg_invalid,
+        cfg_gibberish,
+    }
+
+    parse_ok! {
+        QueueWriterConfiguration,
+        (
+            queue_dir_valid,
+            QueueWriterConfiguration::Directory {
+                path: PathBuf::from("/tmp/yeet/")
+            }
+        ),
+        (
+            queue_pubsub_valid,
+            QueueWriterConfiguration::PubSub {
+                project_id: String::from("asdf123"),
+                credentials_file_path: String::from("/etc/gcloud.json"),
+                topic: String::from("somequeue"),
+            }
+        ),
+    }
+
+    parse_fail! {
+        QueueWriterConfiguration,
+
+        queue_gibberish,
     }
 }
