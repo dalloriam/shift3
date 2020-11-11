beff79b7d4903252fa1dde099f102c1d8697a64c
William Dussault <dalloriam@gmail.com>
2020-11-10 20:28:32 -0500

    Add catch of unwinding panics to export macro

--- plugin-core/src/export.rs
+++ plugin-core/src/export.rs
@@ -3,11 +3,20 @@ macro_rules! export {
     (($($action_struct:ty)*), ($($trigger_struct:ty)*)) => {
         #[no_mangle]
         pub extern "C" fn init_plugin() -> Box<$crate::Plugin> {
-            let mut actions: Vec<Box<dyn $crate::ActionPlugin + 'static>> = vec![$(Box::from(<$action_struct>::default()),)*];
-            let mut triggers: Vec<Box<dyn $crate::TriggerPlugin + 'static>> = vec![$(Box::from(<$trigger_struct>::default()),)*];
-            Box::new(
-                $crate::Plugin::new(actions, triggers)
-            )
+            match std::panic::catch_unwind(|| {
+                let mut actions: Vec<Box<dyn $crate::ActionPlugin + 'static>> = vec![$(Box::from(<$action_struct>::default()),)*];
+                let mut triggers: Vec<Box<dyn $crate::TriggerPlugin + 'static>> = vec![$(Box::from(<$trigger_struct>::default()),)*];
+                Box::new(
+                    $crate::Plugin::new(actions, triggers)
+                )
+            }) {
+                Ok(ptr) => ptr,
+                Err(e) => {
+                    // TODO: Find way of logging this _properly_ in case of error.
+                    eprintln!("FFI: caught unwinding panic in plugin [{}]", env!("CARGO_PKG_NAME"));
+                    Box::new($crate::Plugin::new(Vec::new(), Vec::new()))
+                }
+            }
         }
     }
 }
